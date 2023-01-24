281872cd364117ff65e52029da4bc654453403f8
reshad21 <miakala7@gmail.com>
2023-01-24 14:03:34 +0600

    minimum design complete

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,13 +2,98 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='mx-auto max-w-7xl bg-slate-400'>
-            <div class="flex flex-row justify-between">
+        <div className='mx-auto max-w-7xl py-5'>
+            <div class="flex flex-row justify-between gap-8">
                 <div class="basis-3/4">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eius commodi, animi, possimus fugiat hic placeat nulla quia facilis, voluptatum voluptatem officiis tempora a sequi quidem reiciendis at! Atque, labore?</p>
+                    <div className="grid grid-cols-3 gap-5">
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card bg-base-100 shadow-xl">
+                            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="basis-1/4">
-                    <h1>LATEST EPISODES</h1>
+                    <h1 className='bg-[#0070e8] text-white px-2 py-3 font-semibold'>LATEST EPISODES</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore amet delectus pariatur iure recusandae distinctio nesciunt excepturi laboriosam fugiat eligendi dolor, molestiae doloribus quia ducimus! Iste, id esse. Beatae, facilis.</p>
                 </div>
             </div>
