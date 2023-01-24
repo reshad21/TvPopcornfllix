2780559f68a51de398e9c81c9a20c34fad0abade
reshad21 <miakala7@gmail.com>
2023-01-24 13:18:40 +0600

    design layout create

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mx-auto max-w-7xl bg-slate-400'>
+            <div class="flex flex-row justify-between">
+                <div class="basis-3/4">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo eius commodi, animi, possimus fugiat hic placeat nulla quia facilis, voluptatum voluptatem officiis tempora a sequi quidem reiciendis at! Atque, labore?</p>
+                </div>
+                <div class="basis-1/4">
+                    <h1>LATEST EPISODES</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore amet delectus pariatur iure recusandae distinctio nesciunt excepturi laboriosam fugiat eligendi dolor, molestiae doloribus quia ducimus! Iste, id esse. Beatae, facilis.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
